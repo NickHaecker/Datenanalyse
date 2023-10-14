ac570171d2b1ce69e0e1d08e323372e43cabb557
Nick Häcker <nick.athaeck@googlemail.com>
2023-10-14 20:36:39 +0200

    add value resolver

--- Calculator/Mittelwert.js
+++ Calculator/Mittelwert.js
@@ -1,15 +1,21 @@
-export const values = [24, 27, 58, 26, 24, 27, 30, 24]
+ const values = [24, 27, 58, 26, 24, 27, 30, 24]
 
+ const newValues =  [24, 27, 26, 24, 27, 30, 24]
 
+const isNewVal = true
+
+export function GetValues(){
+    return isNewVal ? newValues : values
+}
 
 
 export function GetAverage() {
     let total = 0
-    values.forEach((v) => {
+    GetValues().forEach((v) => {
         total += v
     })
     
-    return total / values.length;
+    return total / GetValues().length;
 }
 
 
